59272deae35bbd53f2c922fe5e184eb8c0d2b867
Isjhar Kautsar <isjhar@id-clouds.net>
2023-05-03 19:50:11 +0800

    update ci cd script

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 - jwt
 - file logger
 - database migration
+
+## setup cid
+1. copy example.gitlab-ci.yml to .gitlab-ci.yml and adjust file
+2. adjust dockerfile env value
--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
-
 #!/bin/bash
-container_name=template-echo-golang
-local=
+#./run.sh [container-name] [dockerfile] [log-path] [port]
+container_name=$1
+dockerfile=$2
+local=$3
+port=$4
+
 docker stop $container_name
 docker rm $container_name
 docker rmi $(docker images $container_name -q)
-docker build -t $container_name:$1 . || exit 1
+docker build -t $container_name:1.0.0 . || exit 1
 docker run --name $container_name \
     -d \
     --restart always \
     -v $local:/go/src/template-echo-golang/logs \
     -it \
-    -p 8030:1323 \
-    $container_name:$1 || exit 1
+    -p $port:1323 \
+    $container_name:1.0.0 || exit 1
