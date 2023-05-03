e534c067f20adf7cadf3bf79a7fdbf91d0181e77
Isjhar Kautsar <isjhar@id-clouds.net>
2023-05-03 21:52:58 +0800

    dont use param

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#./run.sh [container-name] [dockerfile] [log-path] [port]
-container_name=$1
-dockerfile=$2
-local=$3
-port=$4
+container_name=
+dockerfile=
+local=
+port=
 
 docker stop $container_name
 docker rm $container_name
 docker rmi $(docker images $container_name -q)
-docker build -t $container_name:1.0.0 . || exit 1
+docker build -f $dockerfile -t $container_name:1.0.0 . || exit 1
 docker run --name $container_name \
     -d \
     --restart always \
