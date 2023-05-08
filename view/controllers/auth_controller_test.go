1a61b01022dbe693cdfbfb938256199c71cc1ef5
Isjhar Kautsar <isjhar@id-clouds.net>
2023-05-08 13:41:56 +0800

    add auth test

---
+++ view/controllers/auth_controller_test.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io"
+	"isjhar/template/echo-golang/utils"
+	"isjhar/template/echo-golang/view/dto"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+)
+
+func TestLogin_ReturnSuccess(t *testing.T) {
+	body := dto.LoginParams{
+		Username: "admin",
+		Password: "1234",
+	}
+	bodyJson, _ := json.Marshal(&body)
+
+	e := echo.New()
+	e.Validator = &utils.CustomValidator{
+		Validator: validator.New(),
+	}
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(string(bodyJson)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/auth/login")
+
+	h := Login()
+
+	h(c)
+
+	if http.StatusOK != rec.Code {
+		bodyBytes, err := io.ReadAll(rec.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		t.Fatalf("Call error: %v -> %s", rec.Code, string(bodyBytes))
+	}
+}
