8362c134bd930a62d4d5bd77df0959c1e2b7f747
Isjhar Kautsar <isjhar@gmail.com>
2023-06-12 14:04:42 +0800

    downmigration if exist for testing purpose

--- data/repositories/migration.go
+++ data/repositories/migration.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"errors"
 	"isjhar/template/echo-golang/utils"
 
 	"github.com/golang-migrate/migrate/v4"
@@ -15,10 +16,17 @@ func Migrate() error {
 	if err != nil {
 		return err
 	}
-	err = m.Down()
-	if err != nil {
+	version, _, err := m.Version()
+	if err != nil && !errors.Is(err, migrate.ErrNilVersion) {
 		return err
 	}
+	if version > 0 {
+		err = m.Down()
+		if err != nil {
+			return err
+		}
+	}
+
 	err = m.Up() // or m.Step(2) if you want to explicitly set the number of migrations to run
 	if err != nil {
 		return err
