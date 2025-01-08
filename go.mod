9863561e844586318f5cc45d3a68d7ade3166831
Isjhar Kautsar <isjhar@id-clouds.net>
2025-01-08 18:03:26 +0800

    change main

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/labstack/echo/v4 v4.7.1
 	github.com/leodido/go-urn v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/guregu/null.v4 v4.0.0
 	gorm.io/gorm v1.21.4
--- main.go
+++ main.go
@@ -1,31 +1,68 @@
 package main
 
 import (
+	"isjhar/template/echo-golang/data/repositories"
+	"isjhar/template/echo-golang/docs"
 	"isjhar/template/echo-golang/utils"
+	"isjhar/template/echo-golang/view"
 	"isjhar/template/echo-golang/view/routers"
 	"log"
+	"time"
 
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 )
 
+// @title           Silog API
+// @version         1.0
+// @description     Silog API.
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
+	docs.SwaggerInfo.Title = "Silog API"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = utils.GetBaseUrl()
+	docs.SwaggerInfo.Schemes = []string{"https", "http"}
+
 	e := echo.New()
 	e.HideBanner = true
 	e.Validator = &utils.CustomValidator{
 		Validator: validator.New(),
 	}
-	e.Use(middleware.Recover())
+	e.HTTPErrorHandler = view.CustomHTTPErrorHandler
+
+	buildMiddleware(e)
 
 	environment := utils.GetEnvironment()
-	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmsgprefix)
-	if environment != utils.DEVELOPMENT {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	switch environment {
+	case utils.LOCAL, utils.DEVELOPMENT:
+	case utils.PROD:
+		output := &repositories.ElasticsearchRepository{}
+		log.SetOutput(output)
+	default:
 		output := &utils.CustomLogger{}
 		log.SetOutput(output)
-		utils.LogLevel = utils.LogWarningLevel
 	}
 
+	logLevel := utils.GetLogLevel()
+	if logLevel == utils.LogInfoLevel {
+		utils.LogLevel = utils.LogInfoLevel
+	} else if logLevel == utils.LogWarningLevel {
+		utils.LogLevel = utils.LogWarningLevel
+	} else if logLevel == utils.LogErrorLevel {
+		utils.LogLevel = utils.LogErrorLevel
+	}
+
+	routers.Route(e)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func buildMiddleware(e *echo.Echo) {
 	//CORS Config
 	CORSConfig := middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
@@ -33,7 +70,23 @@ func main() {
 	}
 	e.Use(middleware.CORSWithConfig(CORSConfig))
 
-	routers.Route(e)
+	e.Use(middleware.Recover())
+	e.Use(middleware.RateLimiterWithConfig(middleware.RateLimiterConfig{
+		Skipper: middleware.DefaultSkipper,
+		Store: middleware.NewRateLimiterMemoryStoreWithConfig(
+			middleware.RateLimiterMemoryStoreConfig{Rate: rate.Limit(50), Burst: 30, ExpiresIn: 3 * time.Minute},
+		),
+		IdentifierExtractor: func(ctx echo.Context) (string, error) {
+			id := ctx.RealIP()
+			return id, nil
+		},
+	}))
+	e.Use(view.ElapsedTimeMiddleware())
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Timeout: 1 * time.Minute,
+		Skipper: func(c echo.Context) bool {
+			return c.Request().Header.Get("Upgrade") == "websocket"
+		},
+	}))
 }
