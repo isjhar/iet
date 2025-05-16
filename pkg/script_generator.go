8cfe383ef0287ab69d4c36bb25ef1382a5ae91ab
Isjhar Kautsar <isjhar@id-clouds.net>
2025-05-16 10:55:28 +0800

    create script generator

---
+++ pkg/script_generator.go
@@ -0,0 +1,18 @@
+package pkg
+
+type ScriptGenerator struct {
+}
+
+func (*ScriptGenerator) GenerateGet(modelName string) error {
+	var err error
+
+	rootPath := Find
+	return err
+}
+
+const (
+	entitiyScript = `package entities
+	type User struct {
+		ID       int64
+	}`
+)
